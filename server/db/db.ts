fae70536e2840fe9d25a010b38751f02a43157d0
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-04 12:12:15 +0900

    refactored db.ts

--- server/db/db.ts
+++ server/db/db.ts
@@ -3,9 +3,9 @@ import mysql from "mysql2/promise";
 
 async function createConnection() {
   const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "drizzledb",
+    host: process.env.MYSQL_HOST!,
+    user: process.env.MYSQL_USER!,
+    database: process.env.MYSQL_DB! ,
     port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
