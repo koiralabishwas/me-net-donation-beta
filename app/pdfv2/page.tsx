ff04038dfdff609849a19b2391dc6d23b18295e1
Bishwas Koirala <wasubisu69@gmail.com>
2024-07-27 20:48:54 +0900

    fixed minor errors of ts

--- app/pdfv2/page.tsx
+++ app/pdfv2/page.tsx
@@ -9,7 +9,7 @@ const page = () => {
     <div className='flex w-full content-center'>
       
       {/* BillingTable needs to get rendered first to render DownLoad it */}
-      {<BillingTable/> ? <Button> <DownloadBillingTable  /></Button> : <p>Loading...</p> }
+      <Button> <DownloadBillingTable  /></Button>
     </div>
   )
 }
