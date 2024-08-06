4ba1bfb6ae14c661466b83080967166b8f3ddbd4
Rajan Valencia <47655366+rajanvalencia@users.noreply.github.com>
2024-08-07 02:29:02 +0900

    feat: pdf

--- app/pdfv2/page.tsx
+++ app/pdfv2/page.tsx
@@ -1,34 +1,29 @@
-'use client'
-import React from 'react'
-import dynamic from 'next/dynamic'
-import DownloadBillingTable from './DownloadBillingTable'
-import { Button } from '../components/ui/button'
-import DonationCertificate from './DonationCertificate'
+"use client";
+import React from "react";
+import dynamic from "next/dynamic";
+import DonationCertificate from "./DonationCertificate";
 
 // import pdfViewer to render only in client side
 // SSR is not supported and leads to error
-const PDFViewer = dynamic(() => import('@react-pdf/renderer').then(mod => mod.PDFViewer), {
-  ssr: false,
-})
+const PDFViewer = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+  }
+);
 
 const page = () => {
   return (
-    // <div className='flex w-full content-center'>
-      
-    //   {/* BillingTable needs to get rendered first to render DownLoad it */}
-    //   {/* <Button> <DownloadBillingTable  /></Button> */}
-      
-    // </div>
     <div
       style={{
         height: "100vh",
       }}
     >
-      <PDFViewer width="100%" height="100%">
+      <PDFViewer width="100%" height="100%" style={{ margin: 0 }}>
         <DonationCertificate />
       </PDFViewer>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ app/utils/date.ts
@@ -0,0 +1,19 @@
+/**
+ * @param date
+ * @returns 令和X年Y月Z日
+ */
+export function dateInJapanese(date: Date) {
+  const japaneseDate = new Intl.DateTimeFormat("ja-JP-u-ca-japanese", {
+    dateStyle: "long",
+  }).format(date);
+  return japaneseDate;
+}
+
+/**
+ * @param date
+ * @returns 令和X年
+ */
+export const yearInJapanese = (date: Date) => {
+  const yearIndex = dateInJapanese(date).indexOf("年");
+  return dateInJapanese(date).substring(0, yearIndex + 1);
+};
