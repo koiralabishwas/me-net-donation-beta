a11dcb640e46fd1d6cc8de1daebe2c93fe159a28
Bishwas Koirala <wasubisu69@gmail.com>
2024-08-07 14:56:50 +0900

    drizzle-kit migrate not working properly

--- server/db/db.ts
+++ server/db/db.ts
@@ -1,37 +1,28 @@
-
-import 'dotenv/config'
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
-import * as schema from "./schema"
+import * as schema from "./schema";
+// host: "localhost",
+// user: "root",
+// database: "drizzledb" ,
+// password:"12345",
+// port: 3306,
+// waitForConnections: true,
+// connectionLimit: 10,
+// queueLimit: 0,
+// multipleStatements : true
 
-// export async function connection() {
-//    const connection = await mysql.createConnection({
-//     host: process.env.MYSQL_HOST!,
-//     user: process.env.MYSQL_USER!,
-//     database: process.env.MYSQL_DB! ,
-//     port: 3306,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-//   });
+export const dbParams = {
+  host: process.env.MYSQL_HOST!,
+  user: process.env.MYSQL_USER!,
+  database: process.env.MYSQL_DATABASE!,
+  password: process.env.MYSQL_PASSWORD!,
+  port: Number(process.env.MYSQL_PORT!),
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: true,
+};
 
-//   return drizzle(connection ,{schema ,mode :"default"});
-// }
-
-// export const db = connection();
-
-export const connection = await mysql.createConnection({
-
-    host: "localhost",
-    user: "root",
-    database: "drizzledb" ,
-    password:"12345",
-    port: 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    multipleStatements : true
-  });
-
-
-export const db = drizzle(connection , {schema , mode : "default"})
+export const connection = await mysql.createConnection(dbParams);
+export const db = drizzle(connection, { schema, mode: "default" });
