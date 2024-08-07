3aa3a71a38403354c9981050be4910b815c43396
Bishwas Koirala <wasubisu69@gmail.com>
2024-08-07 17:34:56 +0900

    migration works when db files are stored in env not .env.local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,20 @@
+import { config } from 'process'
 import {defineConfig} from 'drizzle-kit'
+import { env } from 'process'
 
 export default defineConfig({
   schema : './server/db/schema.ts',
   out : './server/db/output',
   dialect : "mysql",
   dbCredentials: {
-    host: process.env.HOST!,
-    user: process.env.USER!,
-    database: process.env.DATABASE!,
-    password: process.env.PASSWORD!,
-    port : Number(process.env.PORT)!,
+    // while in dynamic
+    host: process.env.MYSQL_HOST as string,
+    user: process.env.MYSQL_USER as string,
+    database: process.env.MYSQL_DATABASE as string,
+    password: process.env.MYSQL_PASSWORD as string,
+    port : Number(process.env.MYSQL_PORT),
 
+// ハードコードするときは動きます。
     // host:"localhost",
     // user: "root",
     // database: "drizzledb",
