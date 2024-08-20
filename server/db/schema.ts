8434d3e61887ea176d4fee5d6b37796c90c9661d
Bishwas Koirala <wasubisu69@gmail.com>
2024-08-20 16:46:52 +0900

    removed reference

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -30,6 +30,7 @@ export const donor = mysqlTable(
       length: 100,
     }).notNull(),
     name: varchar("name", { length: 266 }).notNull(),
+    email: varchar("email", { length: 255 }).notNull().unique(),
     phone: varchar("phone", { length: 15 }).notNull(),
     country_code: varchar("country_code", { length: 2 }).notNull(),
     postal_code: varchar("postal_code", { length: 10 }).notNull(),
@@ -50,66 +51,82 @@ export const donor = mysqlTable(
   }
 );
 
-export const subscription = mysqlTable("subscription", {
-  subscription_id: bigint("subscription_id", { mode: "bigint", unsigned: true })
-    .primaryKey()
-    .autoincrement(),
-  subscription_external_id: varchar("subscription", { length: 36 })
-    .notNull()
-    .unique(), //uuid,
-  stripe_subscription_id: varchar("stripe_subscription", { length: 100 })
-    .notNull()
-    .unique(),
-  donor_id: bigint("donor_id", { mode: "bigint", unsigned: true })
-    .notNull()
-    .references(() => donor.donor_id),
-  donor_external_id: varchar("donor_external_id", { length: 36 })
-    .notNull()
-    .references(() => donor.donor_external_id), //uuid,
-  donation_project: varchar("donation_project", { length: 50 }).notNull(),
-  amount: int("amount", { unsigned: true }).notNull(),
-  currency: varchar("currency", { length: 3 }).notNull(),
-  is_cancelled: tinyint("is_cancelled", { unsigned: true })
-    .notNull()
-    .default(0),
-  stripe_subscription_object: json("stripe_subscription_object"),
-  created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
-  updated_at: timestamp("updated_at", { mode: "date" }).onUpdateNow(),
-});
+export const subscription = mysqlTable(
+  "subscription",
+  {
+    subscription_id: bigint("subscription_id", {
+      mode: "bigint",
+      unsigned: true,
+    })
+      .primaryKey()
+      .autoincrement(),
+    subscription_external_id: varchar("subscription", { length: 36 })
+      .notNull()
+      .unique(), //uuid,
+    stripe_subscription_id: varchar("stripe_subscription", { length: 100 })
+      .notNull()
+      .unique(),
+    donor_id: bigint("donor_id", { mode: "bigint", unsigned: true }).notNull(),
+    donor_external_id: varchar("donor_external_id", { length: 36 }).notNull(), //uuid,
+    donation_project: varchar("donation_project", { length: 50 }).notNull(),
+    amount: int("amount", { unsigned: true }).notNull(),
+    currency: varchar("currency", { length: 3 }).notNull(),
+    is_cancelled: tinyint("is_cancelled", { unsigned: true })
+      .notNull()
+      .default(0),
+    stripe_subscription_object: json("stripe_subscription_object"),
+    created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
+    updated_at: timestamp("updated_at", { mode: "date" }).onUpdateNow(),
+  },
+  (table) => {
+    return {
+      donor_external_id_idx: index("donor_external_id_idx").on(
+        table.donor_external_id
+      ),
+    };
+  }
+);
 
-export const transaction = mysqlTable("transaction", {
-  transaction_id: bigint("transaction_id", { mode: "bigint", unsigned: true })
-    .notNull()
-    .autoincrement()
-    .primaryKey(),
-  donation_id: varchar("donation_id", { length: 15 }).notNull(),
-  donor_id: bigint("donor_id", { mode: "bigint", unsigned: true })
-    .notNull()
-    .references(() => donor.donor_id),
-  donor_external_id: varchar("donor_external_id", { length: 36 })
-    .notNull()
-    .references(() => donor.donor_external_id), // uuid
-  //subscription_id入れたいけどWebhookがほぼ同時に来る関係でsubscription_external_idのみにする
-  subscription_external_id: varchar("subscription_external_id", {
-    length: 36,
-  }).references(() => subscription.subscription_external_id), //uuid
-  stripe_subscription_id: varchar("stripe_subscription_id", { length: 100 })
-    .notNull()
-    .references(() => subscription.stripe_subscription_id),
-  donation_project: varchar("donation_project", { length: 50 }).notNull(),
-  amount: int("amount", { unsigned: true }).notNull(),
-  currency: varchar("currency", { length: 3 }).notNull(),
-  type: varchar("type", {
-    length: 20,
-    enum: ["ONE_TIME", "MONTHLY", "YEARLY"],
-  }).notNull(),
-  tax_deduction_certificate_url: varchar("tax_deduction_certificate_url", {
-    length: 1023,
-  }).notNull(),
-  stripe_object: json("stripe_object"), //"一回の場合はpayment_intent、毎月の場合はinvoiceのobjectを保存"
-  created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
-  updated_at: timestamp("updated_at", { mode: "date" }).onUpdateNow(),
-});
+export const transaction = mysqlTable(
+  "transaction",
+  {
+    transaction_id: bigint("transaction_id", { mode: "bigint", unsigned: true })
+      .notNull()
+      .autoincrement()
+      .primaryKey(),
+    donation_id: varchar("donation_id", { length: 15 }).notNull(),
+    donor_id: bigint("donor_id", { mode: "bigint", unsigned: true }).notNull(),
+    donor_external_id: varchar("donor_external_id", { length: 36 }).notNull(), // uuid
+    //subscription_id入れたいけどWebhookがほぼ同時に来る関係でsubscription_external_idのみにする
+    subscription_external_id: varchar("subscription_external_id", {
+      length: 36,
+    }), //uuid
+    stripe_subscription_id: varchar("stripe_subscription_id", {
+      length: 100,
+    }).notNull(),
+    donation_project: varchar("donation_project", { length: 50 }).notNull(),
+    amount: int("amount", { unsigned: true }).notNull(),
+    currency: varchar("currency", { length: 3 }).notNull(),
+    type: varchar("type", {
+      length: 20,
+      enum: ["ONE_TIME", "MONTHLY", "YEARLY"],
+    }).notNull(),
+    tax_deduction_certificate_url: varchar("tax_deduction_certificate_url", {
+      length: 1023,
+    }).notNull(),
+    stripe_object: json("stripe_object"), //"一回の場合はpayment_intent、毎月の場合はinvoiceのobjectを保存"
+    created_at: timestamp("created_at", { mode: "date" }).defaultNow(),
+    updated_at: timestamp("updated_at", { mode: "date" }).onUpdateNow(),
+  },
+  (table) => {
+    return {
+      donation_id_idx: index("donation_id_idx").on(table.donation_id),
+      donor_external_id_idx: index("donor_external_id_idx").on(
+        table.donor_external_id
+      ),
+    };
+  }
+);
 
 export type Donor = typeof donor;
 export type Transaction = typeof transaction;
